068d8b2e350440e2473f12c2130e6cc0655a7913
Raju Acharya <rajuacharya35@hotmail.com>
2020-08-22 17:47:23 +1000

    COmmit of IoC

---
+++ MicroServicesRabbitMQ/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public public Class1()
+	{
+		public static void RegisterServices(IServiceCollection services)
+        {
+			//domain bus
+			services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+	}
+}
