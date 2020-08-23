2e28bfd3abad980314579842b24fbe8a9118ca4d
Raju Acharya <rajuacharya35@hotmail.com>
2020-08-23 16:22:06 +1000

    Banking Microservice - Publish and SenCommand

---
+++ MicroServicesRabbitMQ/MicroRabbit.Banking.Domain/Events/TransferCreatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Events
+{
+    public class TransferCreatedEvent : Event
+    {
+        public int From { get; private set; }
+        public int To { get; private set; }
+
+        public decimal Ammount { get; private set; }
+
+        public TransferCreatedEvent (int from, int to, decimal ammount)
+        {
+            From = from;
+            To = to;
+            Ammount = ammount;
+        }
+    }
+}
